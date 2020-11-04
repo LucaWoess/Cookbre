e806ec1975e78d792fb0f2711ce9db066599ac15
Nicht Luca <55682303+LucaWoess@users.noreply.github.com>
2020-11-04 09:04:13 +0100

    Added sql database statements

---
+++ Datenbank/CookbreRezeptDatenbank.sql
@@ -0,0 +1,25 @@
+create database CookbreRezeptDatenbank;
+use CookbreRezeptDatenbank;
+
+Create table Gericht (
+	Gericht_ID int auto_increment primary key,
+    Gericht_Name varchar(50) not null,
+    Gericht_Kochanleitung varchar(30000) not null,
+    Gerich_Bild blob,
+    Ist_Veggie boolean not null
+);
+
+Create table Zutat (
+	Zutat_ID int auto_increment primary key,
+    Zutat_Name varchar(50) not null,
+    Einheit varchar(25)not null
+);
+
+Create table Menge (
+	Gericht_ID int not null,
+    Zutat_ID int not null,
+    Menge double not null
+);
+
+alter table Menge add constraint Gericht_ID foreign key (Gericht_ID) references Gericht(Gericht_ID);
+alter table Menge add constraint Zutat_ID foreign key (Zutat_ID) references Zutat(Zutat_ID);
