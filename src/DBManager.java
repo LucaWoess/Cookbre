9706ff9a64710abace27bd168176b09f992dd4d8
Luca Woess <luwoess@tsn.at>
2020-11-17 19:56:44 +0100

    Added methods addDish and removeDish

--- src/DBManager.java
+++ src/DBManager.java
@@ -3,9 +3,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBManager {
-	public static Connection getConnection() throws ClassNotFoundException, SQLException{
+	public static Connection getConnection() throws SQLException{
 		Connection con = null;
-		Class.forName("com.mysql.cj.jdbc.Driver");
 		con = DriverManager.getConnection(
 				"jdbc:mysql://localhost:3306", 	//DB
 				"root",							//user
